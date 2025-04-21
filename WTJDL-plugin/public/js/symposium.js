e8c7479a12619537754a054132eb3d74457bf423
tristanriethorst19 <118358529+tristanriethorst19@users.noreply.github.com>
2025-04-21 09:42:23 +0200

    Added comments for UvA admission

--- WTJDL-plugin/public/js/symposium.js
+++ WTJDL-plugin/public/js/symposium.js
@@ -1,16 +1,18 @@
 jQuery(document).ready(function($) {
-    // Check if the .status-afgelopen class is present on the page
+    // Check if the current post has the class `.status-afgelopen` (meaning the event is over)
     if ($('.status-afgelopen').length > 0) {
-        // Hide the button if the class is present
+        // If the post is marked as "afgelopen" (ended), hide the registration button
         $('#registration').hide();
     } else {
-        // Get the current post URL
+        // Otherwise, build the registration URL dynamically
+
+        // Get the current post URL from the browser
         var postUrl = window.location.href;
 
-        // Add /inschrijven to the post URL
+        // Append '/inschrijven' to create the registration URL
         var registrationUrl = postUrl + '/inschrijven';
 
-        // Add the registration URL to the button click event
+        // When the user clicks the registration button, redirect to the registration URL
         $('#registration').click(function() {
             window.location.href = registrationUrl;
         });
